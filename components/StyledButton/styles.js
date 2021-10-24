0c0ea0ed141441bfaf132f76b3bc77f619a11176
humza.mangrio <humza.mangrio@servicenow.com>
2021-10-23 20:46:00 -0700

    Initial sign up screen commit

---
+++ components/StyledButton/styles.js
@@ -0,0 +1,37 @@
+import {StyleSheet} from 'react-native';
+// const RobotoRegular = require('../../assets/fonts/favicon/Roboto-Regular.ttf');
+// import { 
+//     Roboto_100Thin,
+//     Roboto_100Thin_Italic,
+//     Roboto_300Light,
+//     Roboto_300Light_Italic,
+//     Roboto_400Regular,
+//     Roboto_400Regular_Italic,
+//     Roboto_500Medium,
+//     Roboto_500Medium_Italic,
+//     Roboto_700Bold,
+//     Roboto_700Bold_Italic,
+//     Roboto_900Black,
+//     Roboto_900Black_Italic 
+//   } from '@expo-google-fonts/roboto';
+ 
+
+const styles =StyleSheet.create({
+    container: {
+        width: '100%',
+        padding: 10,
+    },
+    button: {
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 20,
+    },
+    text: {
+        fontSize: 16,
+        fontWeight: '400',
+        fontFamily: 'Futura'
+    }
+});
+
+export default styles;
