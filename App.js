222940bb897274c243aa2220c009f526c995162d
Humza Mangrio <humza.mangrio@redfin.com>
2021-11-06 16:02:38 -0700

    Refactored SignupScreen to be its own component

--- App.js
+++ App.js
@@ -2,53 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Button, ImageBackground, StyleSheet, Text, View } from 'react-native';
 import StyledButton from './components/StyledButton';
+import SignupScreen from './screens/SignupScreen';
 
 export default function App() {
 
-  const image = require('./assets/movies_background_underlay.jpg');
-
   return (
-    <ImageBackground source={image} resizeMode='cover' style={styles.image}>
-      <View style={styles.mainContainer}>
-        <Text style={styles.mainTitle}>flixify</Text> 
-      </View>
-      <View style={styles.bottomContainer}>
-        <StyledButton content="Sign up"/>
-        <Text style={styles.loginMessage}>Already have an account? <Text style={styles.loginText}>Login here</Text></Text> 
-        <StatusBar style="auto" />
-      </View>
-    </ImageBackground>
+    <SignupScreen />
   );
 }
 
-const styles = StyleSheet.create({
-  mainContainer: {
-    flex: 1,
-    backgroundColor: 'rgba(0,0,0,0.6)',
-    alignItems: 'center',
-    // paddingTop: 100,
-    justifyContent: 'center',
-  },
-  bottomContainer: {
-    height: 150,
-    backgroundColor: 'rgba(0,0,0,0.6)',
-    alignItems: 'center',
-    paddingBottom: 30,
-  },
-  mainTitle: {
-    fontSize: 75,
-    fontWeight: '500',
-    color: 'white',
-    fontFamily: 'Futura'
-  },
-  image: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  loginMessage: {
-    color: 'white',
-  },
-  loginText: {
-    textDecorationLine: 'underline',
-  }
-});
