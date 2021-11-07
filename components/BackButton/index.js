eaf4979bd2da731f3df1eeaf4ebbf49e56c719bb
Humza Mangrio <humza.mangrio@redfin.com>
2021-11-07 15:31:32 -0800

    Breaking out sign up form to reusable components

---
+++ components/BackButton/index.js
@@ -0,0 +1,27 @@
+import React  from 'react';
+import { Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
+
+
+const BackButton = ({ navigateDest }) => {
+    const navigation = useNavigation();
+
+    return (
+        <>
+            <TouchableOpacity activeOpacity={0.5} onPress={() => navigation.navigate(navigateDest)}>
+                <Image 
+                    source={require('../../assets/icons/back_button.png')}
+                    style={styles.backButton}
+                />
+            </TouchableOpacity>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    backButton: {
+        marginTop: 45,
+    }
+});
+
+export default BackButton;
