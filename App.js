ea00a783ad43ee46436a22fc510f462e6ef94e61
Humza Mangrio <humza.mangrio@redfin.com>
2021-11-06 23:18:45 -0700

    Reconstructed to be a NavigationController

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import { Button, ImageBackground, StyleSheet, Text, View } from 'react-native';
-import StyledButton from './components/StyledButton';
+import EmailScreen from './screens/signup-form/EmailScreen';
 import SignupScreen from './screens/SignupScreen';
 
 export default function App() {
 
+  const Stack = createNativeStackNavigator();
+
+
   return (
-    <SignupScreen />
+    <NavigationContainer>
+      <Stack.Navigator>
+          <Stack.Screen name="Signup" component={SignupScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Email" component={EmailScreen} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
