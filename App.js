d748453f64d55186097dc376e003b71e56a1d824
Humza Mangrio <humza.mangrio@redfin.com>
2021-11-08 00:29:08 -0800

    Added Name screen and Password screen to Navigator

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import EmailScreen from './screens/signup-form/EmailScreen';
+import NameScreen from './screens/signup-form/NameScreen';
+import PasswordScreen from './screens/signup-form/PasswordScreen';
 import SignupScreen from './screens/SignupScreen';
 
 export default function App() {
@@ -14,6 +16,8 @@ export default function App() {
       <Stack.Navigator>
           <Stack.Screen name="Signup" component={SignupScreen} options={{ headerShown: false }} />
           <Stack.Screen name="Email" component={EmailScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Name" component={NameScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Password" component={PasswordScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
